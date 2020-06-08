1474912457c556c2e7222fc45c268a3ed7135734
taqiuddinshokordey <taqiuddinshokordey@gmail.com>
2020-06-09 02:15:15 +0800

    Login  change

--- route/login.js
+++ route/login.js
@@ -54,27 +54,55 @@ router.post('/login',  function (req, res, next) {
 
 
 //Get page after login
- 
-
-router.get('/admin', function(req, res) {
-  if (! req.session.userId ) {
-    var err = new Error("You are not authorized to view this page.");
-    err.status = 403;
-    return next(err);
-  }
-  User.findById(req.session.userId)
-      .exec(function (error, user) {
-        if (error) {
+router.get('/admin',mid, function(req,res){
+    if (! req.session.userId )  
+    {
+      var err = new Error("You are not authorized to view this page.");
+      err.status = 403;
+      return next(err);
+    } else 
+    {
+      User.findById(req.session.userId).exec(function (error, user) {
+        if (error) 
+        {
           return next(error);
-        } else {
+
+        } else
+        {
           console.log(user);
-          return res.render('admin_dashboard',{});
-          
+          return res.render('admin_dashboard',{'user':user});
           ;
         }
       });
+    }
+  
+  
+});
+
+/* router.get('/admin', function(req, res) {
+  if (! req.session.userId ) 
+  {
+    var err = new Error("You are not authorized to view this page.");
+    err.status = 403;
+    return next(err);
+  }else
+  {
+    User.findById(req.session.userId).exec(function (error, user) {
+        if (error) 
+        {
+          return next(error);
+
+        } else
+        {
+          console.log(user);
+          return res.render('admin_dashboard',{});
+          ;
+        }
+      });
+  }
+  
     
-}); 
+}); */ 
 
 
 //get Teacher landing page
